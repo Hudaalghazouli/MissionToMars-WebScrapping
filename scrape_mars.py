93cd14f15594c0a5aa5241247b0915f35c9641aa
Huda Alghazouli <hudaalghazouli@gmail.com>
2022-09-20 20:10:41 -0400

    been working

--- scrape_mars.py
+++ scrape_mars.py
@@ -64,8 +64,8 @@ def scrape_facts_page(browser):
 
     url = 'https://galaxyfacts-mars.com/'
     browser.visit(url)
-    
     html = browser.html
+    
     fact_soup = BeautifulSoup(html, 'html.parser')
     factsLocation = fact_soup.find('div', class_='diagram mt-4')
     factTable = factsLocation.find('table')
