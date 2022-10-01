20d4076684ec8bc27617e8e8d0fecc95881bbec2
Huda Alghazouli <hudaalghazouli@gmail.com>
2022-09-30 23:31:41 -0400

    been working

--- scrape_mars.py
+++ scrape_mars.py
@@ -80,7 +80,7 @@ def scrape_hemisphere(browser):
 
     for i in range(4):
     
-    
+
         hemisphereInfo={}
         
         browser.find_by_css('a.product-item img')[i].click()
@@ -93,7 +93,6 @@ def scrape_hemisphere(browser):
         browser.back()
     
     return hemisphere_image_url
-    
 
 if __name__ == "__main__":
     print(scrape_all)
